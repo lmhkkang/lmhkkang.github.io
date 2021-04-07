f7fd61366fc330dfff36175f11d19080a56b4d93
lmhkkang <37alsgh@gmail.com>
2021-04-07 17:07:04 +0900

    config edit

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
   siteShortTitle: "LeeMinho Portfolio", // Used as logo text in header, footer, and splash screen
   siteDescription:
     "Developer, Lee minho portfolio",
-  siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
+  siteUrl: "https://lmhkkang.github.io/",
   siteLanguage: "en_US",
   siteIcon: "content/favicon.png", // Relative to gatsby-config file
   seoTitleSuffix: "Portfolio Minimal", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
